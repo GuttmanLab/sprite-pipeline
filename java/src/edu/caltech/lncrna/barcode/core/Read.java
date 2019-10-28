851c1a71dd6970f14a4e78ef046fb57db39b5011
Peter <peterch405@gmail.com>
2019-10-27 21:50:34 -0700

    Deleted old version

--- java/src/edu/caltech/lncrna/barcode/core/Read.java
+++
@@ -1,8 +0,0 @@
-package edu.caltech.lncrna.barcode.core;
-
-/**
- * An enumeration to distinguish between read-mates.
- */
-public enum Read {
-    READ1, READ2;
-}
--- java/src/edu/caltech/lncrna/barcode/core/TagCategory.java
+++
@@ -1,8 +0,0 @@
-package edu.caltech.lncrna.barcode.core;
-
-/**
- * An enumeration of tag categories.
- */
-public enum TagCategory {
-    EVEN, ODD, Y, RPM, DPM, LIGTAG, SPACER;
-}
